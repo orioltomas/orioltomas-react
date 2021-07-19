7f0a18c26e3692fb39e7af8c63a38b3477393830
orioltomas <oriol@tomasfortuny.com>
2021-07-20 00:20:51 +0200

    Fix bugs, add projects and add project buttons

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,9 +3,17 @@ function Project ({ project }) {
         <div className="App-project">
             <div className="App-project__name">{ project.name }</div>
             <div className="App-project__description">{ project.description }</div>
-            <a className="App-project__button" href={ project.link } target="_blank" rel="noreferrer">
-                Check it
+            <a className="App-project__button" href={ project.githubLink } target="_blank" rel="noreferrer">
+                <span></span>
+                <span></span>
+                CODE
             </a>
+            { project.appLink !== undefined ? 
+            <a className="App-project__button App-project__button--app" href={ project.appLink } target="_blank" rel="noreferrer">
+                <span></span>
+                <span></span>
+                TRY IT
+            </a> : '' }
         </div>
     )
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,32 +5,34 @@ const projects = [
     {
         name: 'Oriol Tomas website',
         description: 'Website developed with ReactJS, using Javascript and SCSS preprocessor.',
-        link: 'https://github.com/orioltomas/orioltomas-react'
+        githubLink: 'https://github.com/orioltomas/orioltomas-react'
     },
     {
         name: 'Notes application (frontend)',
         description: 'First project developed with ReactJs. Its purpose was to learn some of its basics.',
-        link: 'https://github.com/orioltomas/notes-react'
+        githubLink: 'https://github.com/orioltomas/notes-react'
     },
     {
         name: 'Notes application (backend)',
         description: 'This project led me to develop the notes backend by using Node.js and Express.',
-        link: 'https://github.com/orioltomas/notes-backend'
+        githubLink: 'https://github.com/orioltomas/notes-backend'
     },
     {
         name: 'Tic-tac-toe game',
         description: 'The well known game of tic-tac-toe developed with HTML, Javascript and CSS.\nDownload it and start playing!',
-        link: 'https://github.com/orioltomas/tictactoe-game'
+        githubLink: 'https://github.com/orioltomas/tictactoe-game',
+        appLink: "https://tictactoe-70937.web.app/"
     },
     {
         name: 'Weather application',
         description: 'Developed with HTML, CSS and, mainly, Javascript, this application will let you check the real-time weather in any city of the world!',
-        link: 'https://github.com/orioltomas/weather-app'
+        githubLink: 'https://github.com/orioltomas/weather-app',
+        appLink: "https://weather-app-85b8d.web.app/"
     },
     {
         name: 'Oriol Tomas website (old)',
         description: 'This portfolio page was developed with Symfony framework.',
-        link: 'https://github.com/orioltomas/portfolio'
+        githubLink: 'https://github.com/orioltomas/portfolio'
     }
 ]
 
@@ -41,8 +43,8 @@ function Projects () {
 
     return (
         <div className="App-projects">
-            { showProjects.map(project => 
-                <Project project={ project } />
+            { showProjects.map((project, index) => 
+                <Project key={ index } project={ project } />
             ) }
         </div>
     )
