f2000d7bc477e26f7240d718ea475eacbad124e9
orioltomas <oriol@tomasfortuny.com>
2021-11-25 13:36:27 +0100

    Add ebooks project + various fixes

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,11 @@ import Project from './Project'
 
 const projects = [
     {
+        name: 'Ebooks project',
+        description: 'Project developed with Vue framework (version 3). The aim of this app is to provide a frontend that shows a list of the ebooks where you can search for the one you are looking for and download it. The ebooks and its information is manually maintained within the project.',
+        githubLink: 'https://github.com/orioltomas/ebooks-vue'
+    },
+    {
         name: 'Oriol Tomas website',
         description: 'Website developed with ReactJS, using Javascript and SCSS preprocessor.',
         githubLink: 'https://github.com/orioltomas/orioltomas-react'
@@ -21,13 +26,13 @@ const projects = [
         name: 'Tic-tac-toe game',
         description: 'The well known game of tic-tac-toe developed with HTML, Javascript and CSS.\nDownload it and start playing!',
         githubLink: 'https://github.com/orioltomas/tictactoe-game',
-        appLink: "https://tictactoe-70937.web.app/"
+        appLink: "https://tictactoe.orioltomas.dev/"
     },
     {
         name: 'Weather application',
         description: 'Developed with HTML, CSS and, mainly, Javascript, this application will let you check the real-time weather in any city of the world!',
         githubLink: 'https://github.com/orioltomas/weather-app',
-        appLink: "https://weather-app-85b8d.web.app/"
+        appLink: "https://weather.orioltomas.dev/"
     },
     {
         name: 'Oriol Tomas website (old)',
